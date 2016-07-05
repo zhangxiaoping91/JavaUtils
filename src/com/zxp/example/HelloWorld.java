28e26f43cc56e1516a26cbbc680b5d163a359f05
zhangxiaoping <zhangxiaoping>
2016-07-05 20:13:20 +0800

    让我去

--- src/com/zxp/example/HelloWorld.java
+++ src/com/zxp/example/HelloWorld.java
@@ -1,6 +1,22 @@
 package com.zxp.example;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.junit.Test;
+
+import com.zxp.Json.JsonTransformUtils;
+import com.zxp.bean.Article;
+
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		Article article=new Article("1", "Œ“ «∞≤∑¿");
+		System.out.println(JsonTransformUtils.beanToJson(article));
+	}
+	
+	@Test
+	public void simdate(){
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		System.out.println(sdf.format(new Date(System.currentTimeMillis())));
 	}
 }
